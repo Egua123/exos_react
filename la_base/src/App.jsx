dc4b83a18a18287dd95d4109e627720bfede967f
marioDektop <mariosekpona@gmail.com>
2026-02-02 12:36:12 -0500

    exo1: compteur en utilisant useReducer.

--- la_base/src/App.jsx
+++ la_base/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Counter from './Components/Counter.jsx'
+import Counter from './Counter.jsx'
 
 
 function App() {
